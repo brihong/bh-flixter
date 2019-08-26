aa22de991f554e13eb81b2d08807f7f4e5beabf8
Brian Hong <hong.matchb@gmail.com>
2019-08-26 20:32:28 +0000

    Lessons Show Error

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,22 +1,21 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_enrolled_in_current_course, only: [:show]
+  before_action :require_enrolled_for_current_lesson, only: [:show]
   
   def show
   end
 
   private
 
-def require_enrolled_in_current_course
-  if current_user.enrolled_in?(current_lesson.section.course)
-
-  else
-    redirect_to course_path(current_course), alert: 'Please enroll before viewing this lesson'
+  def require_enrolled_for_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'Please enroll before viewing this lesson'
+    end
   end
-end
 
-  helper_method :current_lesson
+  helper_method :current_course
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
 end
